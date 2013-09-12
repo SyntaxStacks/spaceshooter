d55027b143f5700b2a54407261aa55ea5848fe08
John Lozano <jlozano@peer1.com>
2013-09-11 21:36:44 -0500

    added GameInput.js

---
+++ GameInput.js
@@ -0,0 +1,87 @@
+function GameInput() {
+	var inputQueue = [];
+	var inputCode = {
+		right: 	39,
+		left: 37,
+		space: 32,
+		touch: {
+			left:  'left',
+			right: 'right'
+		}
+	}
+	this.get = getInputs;
+	this.addInput = addInput;
+
+	init();
+
+	function init(){
+		document.addEventListener('keydown', processInput, false);
+		document.addEventListener('keyup', processInput, false);
+	
+		var leftElement = document.getElementById('left');
+		var rightElement = document.getElementById('right');
+
+	    var leftTouch = Hammer(leftElement).on("touch", processInput).on("release", processInput);
+	    var rightTouch = Hammer(rightElement).on("touch", processInput).on("release", processInput);
+	}
+
+	function getInputs() {
+		return inputQueue
+	}
+
+	function addInput(event){
+		for(var i = 0; i < inputQueue.length; i++){
+			if(inputQueue[i].input == event.input){ return;	}
+		}
+		inputQueue.push(event);
+	}
+
+	function removeInput(event){
+		for(var i = 0; i < inputQueue.length; i++){
+			if(inputQueue[i].input == event.input)
+				inputQueue.splice(i,1);
+		}
+	}
+
+	function processInput(event){
+		
+		event.preventDefault();
+
+		if(event.type == 'keydown') {
+			if(event.keyCode == inputCode.right)
+				addInput({input: "RIGHT"});
+
+			if(event.keyCode == inputCode.left)
+				addInput({input: "LEFT"});
+
+			if(event.keyCode == inputCode.space)
+				addInput({input: "SPACE"});
+		}
+		else if(event.type == 'keyup') {
+			if(event.keyCode == inputCode.right)
+				removeInput({input: "RIGHT"});
+			if(event.keyCode == inputCode.left)
+				removeInput({input: "LEFT"});
+			if(event.keyCode == inputCode.space)
+				removeInput({input: "SPACE"});
+		}
+		else if(event.type == 'touch') {
+			if(event.srcElement.id == inputCode.touch.right)
+				addInput({input: "RIGHT"});
+
+			if(event.srcElement.id == inputCode.touch.left)
+				addInput({input: "LEFT"});
+		}
+		else if(event.type == 'release') {
+			if(event.srcElement.id == inputCode.touch.right)				
+				removeInput({input: "RIGHT"});
+			if(event.srcElement.id == inputCode.touch.left)
+				removeInput({input: "LEFT"});
+		}
+
+		console.log(inputQueue);
+	};
+}
+
+
+define(['Hammer'], function(){ return GameInput; });
